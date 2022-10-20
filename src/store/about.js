30ab9959d657b2ddc7ba184f45cbfcf37853fbdb
IHU <2022ihu@gmail.com>
2022-10-20 22:28:45 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'IHU',
+    email: '2022IHU@gmail.com',
+    blog: 'https://IHU/blog',
+    phone: '+82-10-1234-5678',
+    image: 'https://github.com/2022IHU/etc/blob/master/main.png?raw=true'
+  })
+}
